50e819226c758dd4305b8d9b3cbf689e23f7a3e2
cube3x3x3 <cube3x3x3@gmail.com>
2017-05-30 13:08:32 +0900

    Delete temporary test code
    refactoring

--- just-learning-kmeans.py
+++ just-learning-kmeans.py
@@ -7,21 +7,8 @@ Created on Tue May 30 07:45:09 2017
 
 import math
 
-a = (61.0, 100.0)
-b = (64.0, 150.0)
-c = (70.0, 140.0)
-print a,b,c
-
-m = ((a[0]+b[0]+c[0])/3, (a[1]+b[1]+c[1])/3)
-print 'm', m
-
 def dist(m, a):
     return math.sqrt((m[0]-a[0])**2 + (m[1]-a[1])**2)
-    
-    
-print dist(m, a)
-print dist(m, b)
-print dist(m, c)
 
 datasets = (
     (65.0, 220.0 ), ( 73.0, 160.0 ), ( 59.0, 110.0 ), ( 61.0, 120.0 ), 
@@ -30,8 +17,6 @@ datasets = (
     ( 74.0, 170.0 ), ( 70.0, 210.0 ), ( 61.0, 110.0 ), ( 58.0, 100.0 ),
     ( 66.0, 230.0 ), ( 59.0, 120.0 ), ( 68.0, 210.0 ), ( 61.0, 130.0 ) 
 )
-
-print datasets
 
 """
 assign each tuple to a randomly selected cluster
@@ -51,7 +36,6 @@ k = 3
 maxCount = 30
 # assign each tuple to a randomly selected cluster
 clusters = [random.randrange(k) for i in range(len(datasets))]
-print clusters, len(clusters)
 
 # compute the centroid for each cluster
 ## Calculate the new means to be the centroids of the observations
@@ -66,24 +50,17 @@ def updateStep(centroid):
         means[clusters[i]][1] += datasets[i][1]
         clusterCounts[clusters[i]] += 1
     
-    # print means, len(means), clusterCounts, len(clusterCounts)
-    
-    
     for i in range(k):
         means[i][0] /= clusterCounts[i]
         means[i][1] /= clusterCounts[i]
         
-    #print means, len(means)
     nearest = [sys.maxint for i in range(k)]
-    #print nearest, len(nearest), centroid, len(centroid)
     for i in range(len(datasets)):
         # dist(m, a)
         currentDist = dist(means[clusters[i]], datasets[i])
         if currentDist < nearest[clusters[i]]:
             nearest[clusters[i]] = currentDist
             centroid[clusters[i]] = datasets[i]
-    
-    print nearest, len(nearest), centroid, len(centroid)
 
 updateStep(centroid)
 #loop until no improvement or until maxCount
@@ -107,18 +84,18 @@ for n in range(maxCount):
     lastCentroid = centroid[:]
     updateStep(centroid)
     if lastCentroid == centroid:
-        print n, centroid
+        print 'count:', n, 'centroid:', centroid
         break
 
     
 #return clustering
-print clusters, len(clusters)
 
 def printClusters(k, datasets, clusters):
     for i in range(k):
         for j in range(len(datasets)):
             if i == clusters[j]:
-                print i,j,datasets[j]
+                print i, '|', j, '|', datasets[j]
+        print '-'*20
             
 printClusters(k, datasets, clusters)
             

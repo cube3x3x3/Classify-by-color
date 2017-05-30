ca4e1335f591fe9046772dfc30c96cd7ad12430a
cube3x3x3 <cube3x3x3@gmail.com>
2017-05-30 14:09:50 +0900

    refactoring
    naming sytle fixed.

--- just-learning-kmeans.py
+++ just-learning-kmeans.py
@@ -4,21 +4,9 @@ Created on Tue May 30 07:45:09 2017
 
 @author: cube3x3x3
 """
-
-import math
-
-def dist(m, a):
-    return math.sqrt((m[0]-a[0])**2 + (m[1]-a[1])**2)
-
-datasets = (
-    (65.0, 220.0 ), ( 73.0, 160.0 ), ( 59.0, 110.0 ), ( 61.0, 120.0 ), 
-    ( 75.0, 150.0 ), ( 67.0, 240.0 ), ( 68.0, 230.0 ), ( 70.0, 220.0 ),
-    ( 62.0, 130.0 ), ( 66.0, 210.0 ), ( 77.0, 190.0 ), ( 75.0, 180.0 ),
-    ( 74.0, 170.0 ), ( 70.0, 210.0 ), ( 61.0, 110.0 ), ( 58.0, 100.0 ),
-    ( 66.0, 230.0 ), ( 59.0, 120.0 ), ( 68.0, 210.0 ), ( 61.0, 130.0 ) 
-)
-
 """
+k-means
+
 assign each tuple to a randomly selected cluster
 compute the centroid for each cluster
 loop until no improvement or until maxCount
@@ -29,52 +17,66 @@ loop until no improvement or until maxCount
 end loop
 return clustering
 """
+
+import math
 import random
 import sys
 random.seed(12345)
-k = 3
-maxCount = 30
+NUMBER_OF_CLUSTER = 3
+MAX_COUNT = 30
+TEST_DATA_SETS = (
+    (65.0, 220.0 ), ( 73.0, 160.0 ), ( 59.0, 110.0 ), ( 61.0, 120.0 ), 
+    ( 75.0, 150.0 ), ( 67.0, 240.0 ), ( 68.0, 230.0 ), ( 70.0, 220.0 ),
+    ( 62.0, 130.0 ), ( 66.0, 210.0 ), ( 77.0, 190.0 ), ( 75.0, 180.0 ),
+    ( 74.0, 170.0 ), ( 70.0, 210.0 ), ( 61.0, 110.0 ), ( 58.0, 100.0 ),
+    ( 66.0, 230.0 ), ( 59.0, 120.0 ), ( 68.0, 210.0 ), ( 61.0, 130.0 ) 
+)
+
+def dist(m, a):
+    return math.sqrt((m[0]-a[0])**2 + (m[1]-a[1])**2)
+
+
 # assign each tuple to a randomly selected cluster
-clusters = [random.randrange(k) for i in range(len(datasets))]
+clusters = [random.randrange(NUMBER_OF_CLUSTER) for i in range(len(TEST_DATA_SETS))]
 
 # compute the centroid for each cluster
 ## Calculate the new means to be the centroids of the observations
 ##  in the new clusters.
-centroid = [[0,0] for i in range(k)]
+centroid = [[0,0] for i in range(NUMBER_OF_CLUSTER)]
 
-def updateStep(centroid):
-    means = [[0, 0] for i in range(k)]
-    clusterCounts = [0 for i in range(k)]
-    for i in range(len(datasets)):
-        means[clusters[i]][0] += datasets[i][0]
-        means[clusters[i]][1] += datasets[i][1]
-        clusterCounts[clusters[i]] += 1
+def update_step(centroid):
+    _means = [[0, 0] for i in range(NUMBER_OF_CLUSTER)]
+    _clusterCounts = [0 for i in range(NUMBER_OF_CLUSTER)]
+    for i in range(len(TEST_DATA_SETS)):
+        _means[clusters[i]][0] += TEST_DATA_SETS[i][0]
+        _means[clusters[i]][1] += TEST_DATA_SETS[i][1]
+        _clusterCounts[clusters[i]] += 1
     
-    for i in range(k):
-        means[i][0] /= clusterCounts[i]
-        means[i][1] /= clusterCounts[i]
+    for i in range(NUMBER_OF_CLUSTER):
+        _means[i][0] /= _clusterCounts[i]
+        _means[i][1] /= _clusterCounts[i]
         
-    nearest = [sys.maxint for i in range(k)]
-    for i in range(len(datasets)):
+    nearest = [sys.maxint for i in range(NUMBER_OF_CLUSTER)]
+    for i in range(len(TEST_DATA_SETS)):
         # dist(m, a)
-        currentDist = dist(means[clusters[i]], datasets[i])
+        currentDist = dist(_means[clusters[i]], TEST_DATA_SETS[i])
         if currentDist < nearest[clusters[i]]:
             nearest[clusters[i]] = currentDist
-            centroid[clusters[i]] = datasets[i]
+            centroid[clusters[i]] = TEST_DATA_SETS[i]
 
-updateStep(centroid)
+update_step(centroid)
 #loop until no improvement or until maxCount
 #  assign each tuple to best cluster
 #   (the cluster with closest centroid to tuple)
 #  update each cluster centroid
 #   (based on new cluster assignments)
 #end loop
-for n in range(maxCount):
+for n in range(MAX_COUNT):
     #assign each tuple to best cluster
-    for i in range(len(datasets)):
+    for i in range(len(TEST_DATA_SETS)):
         minDist = sys.maxint
         for j in range(len(centroid)):
-            currentDist = dist(centroid[j], datasets[i])
+            currentDist = dist(centroid[j], TEST_DATA_SETS[i])
             if currentDist < minDist:
                 minDist = currentDist
                 clusters[i] = int(j)
@@ -82,21 +84,22 @@ for n in range(maxCount):
     print clusters
     #update each cluster centroid
     lastCentroid = centroid[:]
-    updateStep(centroid)
+    update_step(centroid)
+    #Loop until no improvement or until maxCount
     if lastCentroid == centroid:
-        print 'count:', n, 'centroid:', centroid
+        #print 'count:', n, 'centroid:', centroid
         break
 
     
 #return clustering
 
-def printClusters(k, datasets, clusters):
+def print_clusters(k, data_sets, clusters):
     for i in range(k):
-        for j in range(len(datasets)):
+        for j in range(len(data_sets)):
             if i == clusters[j]:
-                print i, '|', j, '|', datasets[j]
+                print i, '|', j, '|', data_sets[j]
         print '-'*20
             
-printClusters(k, datasets, clusters)
+print_clusters(NUMBER_OF_CLUSTER, TEST_DATA_SETS, clusters)
             
             

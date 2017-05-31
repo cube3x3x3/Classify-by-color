5275b25ae4cda588bbc89e5ceddf12c5af6f417b
cube3x3x3 <cube3x3x3@gmail.com>
2017-05-31 15:56:39 +0900

    add adjust function
    Adjust the number of clusters.
    heuristic?

--- classify-by-color.py
+++ classify-by-color.py
@@ -6,12 +6,14 @@ Created on Mon May 29 14:10:20 2017
 """
 import numpy as np 
 import cv2 
+import math
 
-NUMBER_OF_COLOR = 3
-MAX_COUNT = 10 # Loop limit
-ATTEMPTS = 3 # number of the process (assign each tuple to a randomly selected cluster)
+NUMBER_OF_COLOR = 32
+MAX_COUNT = 30 # Loop limit
+ATTEMPTS = 10 # number of the process (assign each tuple to a randomly selected cluster)
 EPS = 1.0 # Precision
-FILE_NAME = 'lena_std.tif'
+#FILE_NAME = 'Parrots.bmp'
+FILE_NAME = 'color/Pepper.bmp'
 DISPLAY_IT = 1
 HTML_STYLE = 1
 
@@ -36,6 +38,7 @@ def classify_color(file_name='lena_std.tif', display_it=0, number_of_cluster=3, 
         # res to image file shape
         result_img = result_array.reshape((img.shape)) 
         cv2.imshow('result', result_img)
+        cv2.imshow('origin', img)
         #cv2.imshow('lena', img)
         cv2.waitKey(0) 
         cv2.destroyAllWindows()
@@ -49,5 +52,60 @@ def print_colors(colors, html_style=0):
         else:
             print bgr
 
-colors = classify_color(FILE_NAME, DISPLAY_IT, NUMBER_OF_COLOR, MAX_COUNT)
+colors = classify_color(FILE_NAME, not DISPLAY_IT, NUMBER_OF_COLOR, MAX_COUNT)
+print_colors(colors, HTML_STYLE)
+
+# calculate Euclidian distance
+def array_distance(a, b):
+    tmp = 0
+    for dimension in range(len(a)):
+        tmp += (a[dimension]-b[dimension])**2
+    return math.sqrt(tmp)
+    #return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2)
+
+def check_any_distance(colors, limit_dist):
+    out_count = 0
+    colors = np.int32(colors)
+    for i in range(len(colors)):
+       for j in range(i+1, len(colors)):
+            tmp = array_distance(colors[i], colors[j])
+            if tmp < limit_dist:
+                print i, j, tmp
+                out_count += 1
+    return out_count
+
+NUMBER_OF_CHECK = 30
+number_of_classify = NUMBER_OF_COLOR
+independence_of_color = 60 # Euclidian distance nomal = 50, pastel = 10, vivid = 100
+
+# Adjust the number of clusters
+def adjust_cluster(limit_dist=50, number_of_check=10, number_of_classify=NUMBER_OF_COLOR):
+    last_ok = 0
+    for i in range(number_of_check):
+        print i
+        if number_of_classify >  1:
+            print 'cluster:', number_of_classify
+            colors = classify_color(FILE_NAME, not DISPLAY_IT, number_of_classify, MAX_COUNT)
+            # print_colors(colors, not HTML_STYLE)
+        else:
+            print 'hummm', number_of_classify
+
+        out = check_any_distance(colors, limit_dist)
+
+        if out:
+            if out != 1:
+                number_of_classify /= 2
+            else:
+                number_of_classify -= 1
+        else:
+            if last_ok == number_of_classify:
+                print "EULEKA!"
+                break
+            last_ok = number_of_classify
+            number_of_classify += 1
+        
+    return number_of_classify
+
+number_of_classify = adjust_cluster(independence_of_color, NUMBER_OF_CHECK, NUMBER_OF_COLOR)
+colors = classify_color(FILE_NAME, DISPLAY_IT, number_of_classify, MAX_COUNT)
 print_colors(colors, HTML_STYLE)
